e023c8b436152b1831739867edd125d0f97161c5
Mathsx2 <laabc155@gmail.com>
2024-04-27 13:41:34 -0300

    aula Anatomia das Classes

--- primeiro-projeto/src/App.java
+++ primeiro-projeto/src/App.java
@@ -1,5 +1,18 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Olá, DIO!");
+    public static void main(String[] args) {
+
+        String primeiroNome = "Matheus";
+        String segundoNome = "Almeida";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+
     }
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return "Resultado: " + primeiroNome.concat(" ").concat(segundoNome);
 }
+
+
+}
+
