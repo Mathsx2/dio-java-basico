ac569691211031575a494061768a4522cc810d8c
Mathsx2 <laabc155@gmail.com>
2024-04-29 13:50:03 -0300

    operadores aula 6

--- primeiro-projeto/src/Operadores.java
+++ primeiro-projeto/src/Operadores.java
@@ -1,11 +1,40 @@
 public class Operadores {
     public static void main(String[] args) {
-        int numero = - 5;
+      /*   int numero = - 5;
 
         numero = - numero;
 
-        System.out.println(numero);
+        System.out.println(numero); */
         
+        
+    /*  int a, b;
+        a = 6;
+        b = 6;*/
+
+/* Se for para retorna o texto bota STRING, se for numero seria INT */
+
+    /*String resultado = a==b  ? "verdadeiro" : "false"; */
+    /*if(a==b)
+        resultado = "verdadeiro";
+    else
+        resultado = "falso";*/
+
+    /* System.out.println(resultado); */
+
+    //classe Operadores.java
+    int numero1 = 1;
+    int numero2 = 2;
+
+    boolean simNao = numero1 == numero2;
+    System.out.println("num1 é igual a num2? " + simNao);
+
+    if (numero1 < numero2){
+        System.out.println("a nossa condição é verdadeira");
+    }
+
+    simNao = numero1 != numero2;
+    System.out.println("num1 é diferente do num2? " + simNao);
+
     }
 }
 
