ee48163426346e8941705a811766b87a6ee7a89a
Mathsx2 <laabc155@gmail.com>
2024-04-29 13:06:20 -0300

    Operadores

---
+++ primeiro-projeto/src/Operadores.java
@@ -0,0 +1,22 @@
+public class Operadores {
+    public static void main (String[] args){
+
+        //classe Operadores.java
+    String nomeCompleto = "LINGUAGEM" + "JAVA"; //LINGUAGEMJAVA
+            
+        //qual o resultado das expressoes abaixo?
+    String concatenacao = "?"; 
+    
+    concatenacao = 1+1+1+"1"; //31
+    
+    concatenacao = 1+"1"+1+1; //1111
+    
+    concatenacao = 1+"1"+1+"1"; //1111
+    
+    concatenacao = "1"+1+1+1; //1111
+    
+    concatenacao = "1"+(1+1+1); //13
+    }
+}
+
+
